062f1b864a9fd09768579c0c312044a43ddaa794
Todd Pickell <myappleguy@gmail.com>
2013-09-15 19:19:24 -0500

    got messages going to all sockets now, next would like to get usernames from each socket then display by each message

--- lib/ws_server.rb
+++ lib/ws_server.rb
@@ -2,34 +2,41 @@ require 'em-websocket'
 require 'json'
 require_relative 'converter'
 
+@sockets = []
+
 EM.run {
   puts 'starting server'
   EM::WebSocket.run(:host => "0.0.0.0", :port => 1337) do |ws|
   ws.onopen { |handshake|
+    @sockets << ws
     puts "WebSocket connection open"
-
-    # Access properties on the EM::WebSocket::Handshake object, e.g.
-    # path, query_string, origin, headers
-
-    # Publish message to the client
-    ws.send "Hello Client, you connected to #{handshake.path}"
-    ws.send "Query: #{handshake.query_string}"
   }
 
-  ws.onclose { puts "Connection closed" }
+  ws.onclose {
+    puts "Connection closed"
+    @sockets.delete ws
+  }
 
   ws.onmessage { |msg|
     puts "Recieved: #{msg}"
-    ws.send handle_message(msg)
+    response = handle_message(msg)
+    @sockets.each do |ws|
+      ws.send response
+    end
   }
   end
 
   def handle_message(msg)
     @hashey = JSON.parse(msg)
     return "HASH: #{@hashey}" if false
+    return chat(@hashey) if @hashey.has_key? 'chat'
     return new_it_up(@hashey) if @hashey.has_key? 'new'
     return pass_the_message(@hashey) if @hashey.has_key? 'message'
     'hash didn\'t have any known keys'
+  end
+
+  def chat(hashey)
+    hashey['chat']
   end
 
   def new_it_up(hashey)
